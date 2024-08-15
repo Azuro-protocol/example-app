1a28220caab5e3697a3be50871dac6bf95d266a9
CleanBread <gleb4ist@gmail.com>
2024-08-15 14:04:39 +0200

    input errors

--- src/compositions/Betslip/components/AmountInput/AmountInput.tsx
+++ src/compositions/Betslip/components/AmountInput/AmountInput.tsx
@@ -1,5 +1,6 @@
-import { useChain, useDetailedBetslip } from '@azuro-org/sdk'
+import { useChain, useDetailedBetslip, BetslipDisableReason } from '@azuro-org/sdk'
 import React from 'react'
+import cx from 'classnames'
 import { constants } from 'helpers'
 
 import { Icon } from 'components/ui'
@@ -7,7 +8,7 @@ import { Icon } from 'components/ui'
 
 const AmountInput = () => {
   const { appChain } = useChain()
-  const { betAmount, changeBetAmount } = useDetailedBetslip()
+  const { betAmount, changeBetAmount, disableReason } = useDetailedBetslip()
 
   const handleChange = (value: string) => {
     if (!new RegExp('^(|0|0\\.[0-9]*|[1-9][0-9]*\\.?[0-9]*)$').test(value)) {
@@ -27,19 +28,27 @@ const AmountInput = () => {
     handleChange(event.target.value)
   }
 
+  const isError = [
+    BetslipDisableReason.BetAmountGreaterThanMaxBet,
+    BetslipDisableReason.BetAmountLowerThanMinBet,
+  ].includes(disableReason!)
+
+  const labelClassName = cx('flex items-center px-3 border rounded-t-sm rounded-b-ssm h-10 cursor-text', {
+    'border-accent-red': isError,
+    'border-grey-20': !isError,
+  })
+
   return (
-    <div>
-      <label className="flex items-center px-3 border border-grey-20 rounded-t-sm rounded-b-ssm h-10 cursor-text">
-        <Icon className="size-5 mr-2" name={constants.currencyIcons[appChain.id]} />
-        <input
-          className="bg-transparent w-full h-full placeholder:text-grey-60 text-caption-14 font-semibold"
-          type="number"
-          value={betAmount}
-          onChange={handleInputChange}
-          placeholder="0.00"
-        />
-      </label>
-    </div>
+    <label className={labelClassName}>
+      <Icon className="size-5 mr-2" name={constants.currencyIcons[appChain.id]} />
+      <input
+        className="bg-transparent w-full h-full placeholder:text-grey-60 text-caption-14 font-semibold"
+        type="number"
+        value={betAmount}
+        onChange={handleInputChange}
+        placeholder="0.00"
+      />
+    </label>
   )
 }
 

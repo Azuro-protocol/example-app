c21f1aedbca82be84d666c0154a2d5c82a59528e
CleanBread <gleb4ist@gmail.com>
2025-04-22 09:26:47 +0200

    upgrade @azuro-org/sdk

--- local_modules/wallet/chains.ts
+++ local_modules/wallet/chains.ts
@@ -3,4 +3,4 @@ import { polygonAmoy, spicy, polygon, gnosis, chiliz, base, baseSepolia } from '
 
 const isDevEnabled = Boolean(JSON.parse(process.env.AZURO_UNSTABLE_DEV_ENABLED || 'false'))
 
-export const appChains = isDevEnabled ? [ polygonAmoy, gnosis, spicy, baseSepolia ] as const : [ polygon, polygonAmoy, gnosis, chiliz, spicy, base ] as const
+export const appChains = isDevEnabled ? [ polygonAmoy, spicy, baseSepolia ] as const : [ polygon, gnosis, chiliz, base ] as const

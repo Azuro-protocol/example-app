e9b30857a198138a5802680b5e7234821c6cec3d
decentralisednews <press@decentralised.news>
2025-05-05 06:06:32 +0200

    Update Header.tsx

--- src/compositions/PageLayout/components/Content/components/Header/Header.tsx
+++ src/compositions/PageLayout/components/Content/components/Header/Header.tsx
@@ -1,62 +1,63 @@
-"use client";
+'use client'
 
-import React, { useEffect, useRef, useState } from "react";
-import { usePathname } from "next/navigation";
+import React, { useEffect, useRef, useState } from 'react'
+import { usePathname } from 'next/navigation'
 // import { openModal } from '@locmod/modal'
-import { useWallet } from "wallet";
-import { usePrivy } from "@privy-io/react-auth";
-import { useFreezeBodyScroll } from "hooks";
-import { Icon, Logo } from "components/ui";
-import { Button, buttonMessages } from "components/inputs";
-import Navigation from "compositions/Navigation/Navigation";
-import LiveSwitcher from "compositions/LiveSwitcher/LiveSwitcher";
-import Controls from "../Controls/Controls";
+import { useWallet } from 'wallet'
+import { usePrivy } from '@privy-io/react-auth'
+import { useFreezeBodyScroll } from 'hooks'
+import { Icon, Logo } from 'components/ui'
+import { Button, buttonMessages } from 'components/inputs'
+import Navigation from 'compositions/Navigation/Navigation'
+import LiveSwitcher from 'compositions/LiveSwitcher/LiveSwitcher'
+import Controls from '../Controls/Controls'
+
 
 const Content: React.FC = () => {
-  useFreezeBodyScroll();
+  useFreezeBodyScroll()
 
   return (
     <div className="fixed top-[54px] bottom-0 left-0 nr:w-[22.5rem] mb:w-full bg-bg-l0 overflow-auto no-scrollbar">
       <LiveSwitcher />
       <Navigation className="mt-2" />
     </div>
-  );
-};
+  )
+}
 
 const Header: React.FC = () => {
-  const { account, isReconnecting, isConnecting } = useWallet();
-  const pathname = usePathname();
-  const { login } = usePrivy();
-  const [isVisible, setVisibility] = useState(false);
-  const containerRef = useRef<HTMLDivElement>(null);
+  const { account, isReconnecting, isConnecting } = useWallet()
+  const pathname = usePathname()
+  const { login } = usePrivy()
+  const [isVisible, setVisibility] = useState(false)
+  const containerRef = useRef<HTMLDivElement>(null)
 
   const handleClick = () => {
-    setVisibility((v) => !v);
-  };
+    setVisibility((v) => !v)
+  }
 
   useEffect(() => {
     if (isVisible) {
       const handleOutsideClick = (event: MouseEvent) => {
-        const composedPath = event.composedPath();
+        const composedPath = event.composedPath()
 
         if (!composedPath.includes(containerRef.current!)) {
-          setVisibility(false);
+          setVisibility(false)
         }
-      };
+      }
 
-      document.addEventListener("click", handleOutsideClick, { capture: true });
+      document.addEventListener("click", handleOutsideClick, { capture: true })
 
       return () => {
         document.removeEventListener("click", handleOutsideClick, {
           capture: true,
-        });
-      };
+        })
+      }
     }
-  }, [isVisible]);
+  }, [isVisible])
 
   useEffect(() => {
-    setVisibility(false);
-  }, [pathname]);
+    setVisibility(false)
+  }, [pathname])
 
   return (
     <div ref={containerRef} className="py-2 px-5 bg-bg-l0">
@@ -84,7 +85,7 @@ const Header: React.FC = () => {
       </div>
       {isVisible && <Content />}
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header

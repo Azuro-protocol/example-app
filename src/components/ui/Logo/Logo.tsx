f5608f84f2189023b1ac8ef0089d4462eace3fc2
decentralisednews <press@decentralised.news>
2025-05-05 05:51:45 +0200

    Update Logo.tsx

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,26 +1,27 @@
-import cx from "classnames";
-import Image from "next/image";
-import { Href } from "components/navigation";
+import cx from 'classnames'
+import Image from 'next/image'
+import { Href } from 'components/navigation'
+
 
 type LogoProps = {
-  className?: string;
-};
+  className?: string
+}
 
 const Logo: React.FC<LogoProps> = (props) => {
-  const { className } = props;
+  const { className } = props
 
   return (
-    <Href to="/" className={cx("flex items-center", className)}>
+    <Href to='/' className={cx('flex items-center', className)}>
       <Image
-        className="h-full"
-        src="/images/logo.png"
-        alt="Smart contracts bets"
+        className='h-full'
+        src='/images/logo.png'
+        alt='Smart contracts bets'
         width={710}
         height={160}
         unoptimized
       />
     </Href>
-  );
-};
+  )
+}
 
 export default Logo;

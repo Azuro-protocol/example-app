2d530fc8ff85604640c8002e86ce79b556a50d81
xsunn3 <blazewx@proton.me>
2025-12-18 08:08:57 +0100

    Update Logo.tsx

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -12,119 +12,57 @@ const Logo: React.FC<LogoProps> = (props) => {
 
   return (
     <Href to="/" className={cx('flex items-center', className)}>
-      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" x="0" y="0" width="71" height="16" viewBox="64.32291198730469 105.02180267333985 211.35419128417968 39.95639846801758" preserveAspectRatio="xMidYMid meet" color-interpolation-filters="sRGB">
-  <g>
-    <defs>
-      <linearGradient id="92" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" stop-color="#fa71cd"></stop>
-        <stop offset="100%" stop-color="#9b59b6"></stop>
-      </linearGradient>
-      <linearGradient id="93" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" stop-color="#f9d423"></stop>
-        <stop offset="100%" stop-color="#f83600"></stop>
-      </linearGradient>
-      <linearGradient id="94" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" stop-color="#0064d2"></stop>
-        <stop offset="100%" stop-color="#1cb0f6"></stop>
-      </linearGradient>
-      <linearGradient id="95" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" stop-color="#f00978"></stop>
-        <stop offset="100%" stop-color="#3f51b1"></stop>
-      </linearGradient>
-      <linearGradient id="96" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" stop-color="#7873f5"></stop>
-        <stop offset="100%" stop-color="#ec77ab"></stop>
-      </linearGradient>
-      <linearGradient id="97" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" stop-color="#f9d423"></stop>
-        <stop offset="100%" stop-color="#e14fad"></stop>
-      </linearGradient>
-      <linearGradient id="98" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" stop-color="#009efd"></stop>
-        <stop offset="100%" stop-color="#2af598"></stop>
-      </linearGradient>
-      <linearGradient id="99" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" stop-color="#ffcc00"></stop>
-        <stop offset="100%" stop-color="#00b140"></stop>
-      </linearGradient>
-      <linearGradient id="100" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" stop-color="#d51007"></stop>
-        <stop offset="100%" stop-color="#ff8177"></stop>
-      </linearGradient>
-      <linearGradient id="102" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" stop-color="#a2b6df"></stop>
-        <stop offset="100%" stop-color="#0c3483"></stop>
-      </linearGradient>
-      <linearGradient id="103" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" stop-color="#7ac5d8"></stop>
-        <stop offset="100%" stop-color="#eea2a2"></stop>
-      </linearGradient>
-      <linearGradient id="104" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" stop-color="#00ecbc"></stop>
-        <stop offset="100%" stop-color="#007adf"></stop>
-      </linearGradient>
-      <linearGradient id="105" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" stop-color="#b88746"></stop>
-        <stop offset="100%" stop-color="#fdf5a6"></stop>
-      </linearGradient>
-    </defs>
-    <g fill="#FFFFFF" class="iconlinesvg-g iconlinesvg" transform="translate(66.39500427246094,107.09667106624693)">
-      <g class="tp-name">
-        <g transform="translate(0, 0)">
-          <g fill="#FFFFFF" transform="scale(1.02)">
-            <path d="M4.26-35.11L4.26-35.11L4.55-35.11Q7.22-35.11 8.59-32.14L8.59-32.14Q8.81-31.39 8.81-30.70L8.81-30.70L8.81-24.14Q28.15-34.20 29.92-34.96L29.92-34.96Q30.43-35.11 30.90-35.11L30.90-35.11L31.39-35.11Q34.20-35.11 35.43-31.88L35.43-31.88Q35.50-31.48 35.50-31.24L35.50-31.24L35.50-30.28Q35.50-27.98 32.37-26.49L32.37-26.49Q15.23-17.58 15.11-17.58L15.11-17.58L15.11-17.53Q33.03-8.35 33.96-7.74L33.96-7.74Q35.50-6.19 35.50-4.82L35.50-4.82L35.50-3.87Q35.50-1.64 32.78-0.27L32.78-0.27Q32.07 0 31.51 0L31.51 0L30.75 0Q29.67 0 24.95-2.62L24.95-2.62L8.81-10.97L8.81-4.41Q8.81-1.30 5.53-0.15L5.53-0.15Q4.99 0 4.55 0L4.55 0L4.41 0Q1.15 0 0.07-3.45L0.07-3.45Q0-3.72 0-3.99L0-3.99L0-31.12Q0-33.20 2.37-34.62L2.37-34.62Q3.33-35.11 4.26-35.11Z" transform="translate(0, 35.10728586350976)"></path>
-          </g>
+     <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="71"
+    height="16"
+    colorInterpolationFilters="sRGB"
+    version="1"
+    viewBox="64.323 105.022 211.354 39.956"
+  >
+    <g fill="#FFF" className="iconlinesvg-g iconlinesvg">
+      <g className="tp-name">
+        <path d="M70.74 107.094h.296q2.723 0 4.12 3.03.225.764.225 1.468v6.691q19.727-10.26 21.532-11.036.52-.153 1-.153h.5q2.866 0 4.12 3.295.072.408.072.652v.98q0 2.346-3.193 3.865-17.482 9.089-17.605 9.089v.05q18.279 9.364 19.227 9.986 1.571 1.581 1.571 2.979v.969q0 2.274-2.774 3.672-.725.275-1.296.275h-.775q-1.102 0-5.916-2.672l-16.463-8.517v6.69q0 3.173-3.345 4.346-.551.153-1 .153h-.143q-3.325 0-4.427-3.519-.071-.275-.071-.55v-27.673q0-2.122 2.417-3.57.98-.5 1.928-.5"></path>
+        <g className="imagesvg" transform="translate(106.395 107.512)">
+          <path
+            fill="none"
+            d="M0 0h34.378v34.978H0z"
+            className="image-rect"
+          ></path>
+          <svg
+            width="34.378"
+            height="34.978"
+            className="image-svg-svg primary"
+            filter="url(#colors8200565747)"
+            overflow="visible"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              xmlSpace="preserve"
+              fillRule="evenodd"
+              clipRule="evenodd"
+              imageRendering="optimizeQuality"
+              shapeRendering="geometricPrecision"
+              textRendering="geometricPrecision"
+              viewBox="469374 390357 9061134 9219404"
+            >
+              <path d="M9272317 5308152c-405594-2531713-58701-3902865 258191-4917795-1006071 342287-2368601 723786-4909763 382208C288191 189923 111615 5101812 3589280 5694494c-1003236 758037-2098245 955991-2839983 777762l-279923 287128 2923015 2850377 279923-287129c-197127-737249-26575-1836628 705596-2858880 680203 3461247 5586186 3161009 4894291-1155482z"></path>
+            </svg>
+          </svg>
+          <defs>
+            <filter id="colors8200565747">
+              <feColorMatrix
+                className="icon-feColorMatrix"
+                values="0 0 0 0 0.078125 0 0 0 0 0.94140625 0 0 0 0 0.58203125 0 0 0 1 0"
+              ></feColorMatrix>
+            </filter>
+          </defs>
         </g>
-        <g transform="translate(40, 0.4156109431873922)">
-          <g>
-            <g class="imagesvg">
-              <g>
-                <rect fill="#FFFFFF" fill-opacity="0" stroke-width="2" x="0" y="0" width="34.37763383395224" height="34.97820969440517" class="image-rect"></rect> <svg filter="url(#colors8200565747)" x="0" y="0" width="34.37763383395224" height="34.97820969440517" filtersec="colorsf8105830866" class="image-svg-svg primary" style="overflow: visible;"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" version="1.1" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="469374 390357 9061134 9219404" x="0px" y="0px">
-                    <g>
-                      <path d="M9272317 5308152c-405594,-2531713 -58701,-3902865 258191,-4917795l0 0c0,0 0,0 0,0l0 0c-1006071,342287 -2368601,723786 -4909763,382208 -4332554,-582642 -4509130,4329247 -1031465,4921929 -1003236,758037 -2098245,955991 -2839983,777762l-279923 287128 2923015 2850377 279923 -287129c-197127,-737249 -26575,-1836628 705596,-2858880 680203,3461247 5586186,3161009 4894291,-1155482l118 -118z"></path>
-                    </g>
-                  </svg></svg>
-                <defs>
-                  <filter id="colors8200565747">
-                    <feColorMatrix type="matrix" values="0 0 0 0 0.078125  0 0 0 0 0.94140625  0 0 0 0 0.58203125  0 0 0 1 0" class="icon-feColorMatrix "></feColorMatrix>
-                  </filter>
-                  <filter id="colorsf8105830866">
-                    <feColorMatrix type="matrix" values="0 0 0 0 0.99609375  0 0 0 0 0.99609375  0 0 0 0 0.99609375  0 0 0 1 0" class="icon-fecolormatrix"></feColorMatrix>
-                  </filter>
-                  <filter id="colorsb3775150174">
-                    <feColorMatrix type="matrix" values="0 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 0" class="icon-fecolormatrix"></feColorMatrix>
-                  </filter>
-                </defs>
-              </g>
-            </g>
-          </g>
-        </g>
-        <g transform="translate(78, 0)">
-          <g fill="#FFFFFF" transform="scale(1.02)">
-            <path d="M12.19-35.11L12.19-35.11L31.46-35.11Q34.15-35.11 35.43-32.19L35.43-32.19Q35.50-31.83 35.50-31.58L35.50-31.58L35.50-30.26Q35.50-28.20 32.90-26.73L32.90-26.73Q31.88-26.39 31.24-26.39L31.24-26.39L8.86-26.39L8.86-21.98L22.18-21.98Q30.28-21.98 34.25-16.87L34.25-16.87Q35.50-14.93 35.50-13.61L35.50-13.61L35.50-8.37Q35.50-5.31 31.39-2.35L31.39-2.35Q27.44 0 23.65 0L23.65 0L3.84 0Q1.84 0 0.34-2.28L0.34-2.28Q0-3.01 0-3.60L0-3.60L0-4.55Q0-7.22 3.13-8.50L3.13-8.50Q3.84-8.72 4.75-8.72L4.75-8.72L26.71-8.72L26.71-13.12L12.63-13.12Q6.76-13.12 2.52-16.79L2.52-16.79Q0-19.32 0-21.91L0-21.91L0-26.34Q0-30.65 5.92-33.74L5.92-33.74Q9.38-35.11 12.19-35.11Z" transform="translate(0, 35.10728586350976)"></path>
-          </g>
-        </g>
-        <g transform="translate(118, 0)">
-          <g fill="#FFFFFF" transform="scale(1.02)">
-            <path d="M4.24-35.11L4.24-35.11L4.90-35.11Q7.15-35.11 8.64-32.61L8.64-32.61Q9.06-31.80 9.06-30.68L9.06-30.68L9.06-4.21Q9.06-1.35 5.68-0.07L5.68-0.07Q5.29 0 5.04 0L5.04 0L4.04 0Q1.74 0 0.29-2.69L0.29-2.69Q0-3.40 0-3.94L0-3.94L0-31.02Q0-33.69 3.23-34.96L3.23-34.96Q3.77-35.11 4.24-35.11Z" transform="translate(0, 35.10728586350976)"></path>
-          </g>
-        </g>
-        <g transform="translate(131, 0)">
-          <g fill="#FFFFFF" transform="scale(1.02)">
-            <path d="M4.11-35.11L4.11-35.11L4.75-35.11Q6.63-35.11 8.59-32.90L8.59-32.90Q20.93-20.76 26.69-15.20L26.69-15.20L26.69-30.41Q26.69-33.69 29.70-34.91L29.70-34.91Q30.36-35.11 30.90-35.11L30.90-35.11L31.51-35.11Q33.76-35.11 35.23-32.49L35.23-32.49Q35.50-31.68 35.50-31.24L35.50-31.24L35.50-3.79Q35.50-1.76 32.98-0.34L32.98-0.34Q32.27 0 31.66 0L31.66 0L30.68 0Q28.79 0 26.00-3.26L26.00-3.26L8.81-20.05L8.81-4.21Q8.81-1.35 5.51-0.07L5.51-0.07Q5.14 0 4.90 0L4.90 0L3.92 0Q1.69 0 0.27-2.69L0.27-2.69Q0-3.40 0-3.94L0-3.94L0-31.02Q0-33.69 3.13-34.96L3.13-34.96Q3.67-35.11 4.11-35.11Z" transform="translate(0, 35.10728586350976)"></path>
-          </g>
-        </g>
-        <g transform="translate(171, 0)">
-          <g fill="#FFFFFF" transform="scale(1.02)">
-            <path d="M12.22-35.11L12.22-35.11L23.16-35.11Q29.26-35.11 33.61-30.90L33.61-30.90Q35.50-28.50 35.50-26.69L35.50-26.69L35.50-8.42Q35.50-5.73 32.07-2.84L32.07-2.84Q27.86 0 22.94 0L22.94 0L12.68 0Q6.73 0 2.52-3.65L2.52-3.65Q0-6.17 0-8.76L0-8.76L0-26.34Q0-30.68 5.92-33.74L5.92-33.74Q9.38-35.11 12.22-35.11ZM26.98-26.42L8.52-26.42L8.52-8.69L26.98-8.69L26.98-26.42Z" transform="translate(0, 35.10728586350976)"></path>
-          </g>
-        </g>
+        <path d="M156.829 107.094h19.655q2.744 0 4.05 2.978.071.368.071.623v1.346q0 2.101-2.652 3.6-1.04.347-1.693.347h-22.828v4.499h13.587q8.262 0 12.311 5.212 1.275 1.979 1.275 3.325v5.345q0 3.12-4.192 6.14-4.03 2.397-7.895 2.397h-20.206q-2.04 0-3.57-2.325-.347-.745-.347-1.347v-.969q0-2.723 3.193-4.029.724-.224 1.652-.224h22.4v-4.488h-14.362q-5.988 0-10.313-3.744-2.57-2.58-2.57-5.222v-4.519q0-4.396 6.038-7.548 3.53-1.397 6.396-1.397M188.72 107.094h.673q2.295 0 3.815 2.55.428.826.428 1.969v26.999q0 2.917-3.447 4.223-.398.071-.653.071h-1.02q-2.346 0-3.825-2.744-.296-.724-.296-1.275v-27.621q0-2.724 3.295-4.02.55-.152 1.03-.152M201.587 107.094h.653q1.918 0 3.917 2.254 12.587 12.383 18.462 18.054v-15.514q0-3.346 3.07-4.59.673-.204 1.224-.204h.622q2.295 0 3.795 2.672.275.827.275 1.275v28q0 2.07-2.57 3.518-.725.347-1.347.347h-1q-1.927 0-4.773-3.325l-17.534-17.126v16.157q0 2.917-3.366 4.223-.377.071-.622.071h-1q-2.274 0-3.723-2.744-.275-.724-.275-1.275v-27.621q0-2.724 3.193-4.02.55-.152 1-.152M249.86 107.094h11.158q6.222 0 10.66 4.294 1.927 2.448 1.927 4.294v18.636q0 2.744-3.499 5.691-4.294 2.897-9.312 2.897h-10.465q-6.07 0-10.364-3.723-2.57-2.57-2.57-5.212v-17.932q0-4.426 6.038-7.548 3.53-1.397 6.426-1.397m15.055 8.864h-18.83v18.084h18.83z"></path>
       </g>
     </g>
-    <defs v-gra="od"></defs>
-  </g>
-</svg>
-  )
-}
+  </svg>
+);
 
-export default Logo
+export default SvgIcon;

d8a08aefe1b33eec8a2ca484a023c3a78a5bd647
xsunn3 <blazewx@proton.me>
2025-12-18 09:47:50 +0100

    Update Logo.tsx

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -9,8 +9,8 @@ const Logo: React.FC<LogoProps> = ({ className }) => (
   <Href to="/" className={cx('flex items-center', className)}>
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="120"
-      height="33"
+      width="100"
+      height="40"
       viewBox="64.323 105.022 211.354 39.956"
       colorInterpolationFilters="sRGB"
     >

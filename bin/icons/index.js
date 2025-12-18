cc8b184bd89f0b85c2a5db49a11db4c29cd2310d
xsunn3 <blazewx@proton.me>
2025-12-18 21:44:37 +0100

    Update index.js

--- bin/icons/index.js
+++ bin/icons/index.js
@@ -1,14 +1,2 @@
 require('../bootstrap')
 require('./icons')
-import React from 'react';
-import Logo from '../components/Logo';
-
-export default function Home() {
-  return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', gap: '1rem', padding: '1rem' }}>
-      <Logo />
-      <button className="btn btn-primary">Click me</button>
-    </div>
-  );
-}
-
